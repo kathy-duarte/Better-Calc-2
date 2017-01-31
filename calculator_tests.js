8189a7acca0e4b020fb40e23c26d2688bd1df543
Ryan Q Moynihan <rm73819@srcs.loc>
2017-01-31 08:59:39 -0800

    FIrst test done

--- calculator_tests.js
+++ calculator_tests.js
@@ -2,7 +2,7 @@
 QUnit.test( "Add digits test", function( assert ) {
     addDigit('1');
     addDigit('2');
-    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+    assert.deepEqual(document.getElementById("screen").value, "12", "Passed - Expected 12");
 });
 
 // Test adding one, then two decimals
@@ -10,5 +10,10 @@ QUnit.test( "Add decimal test", function( assert ) {
     addDecimal();
     addDigit('2');
     addDecimal();
-    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+    assert.deepEqual(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
 });
+//Test for inputing digits
+QUnit.test( "Change sign test", function( assert){
+    addDigit('2');
+    assert.deepEqual(document.getElementById("screen").value, "-2", "Passed - Expected -2")
+})
